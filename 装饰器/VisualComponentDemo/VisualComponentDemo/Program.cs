dc195f022f9b7a8a1cc2f407c657785ac5ac6d0a
china-renqing <321589528@qq.com>
2021-05-21 22:43:12 +0800

    简单装饰器案例（组件）

---
+++ 装饰器/VisualComponentDemo/VisualComponentDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualComponentDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //生成一个LIstBox
+            VisualComponent entity = new ListBox();
+            //为ListBox添加Border
+            VisualComponent entity2 = new BorderDecorator(entity);
+
+            //为ListBox添加Scroll
+            VisualComponent entity3 = new ScrollBarDecorator(entity2);
+            //在这里进行渲染
+            entity3.Display();
+            Console.Read();
+        }
+    }
+}
