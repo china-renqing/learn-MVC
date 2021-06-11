273359bd57aa5a9721d9c384b732f5b4e7f50959
china-renqing <321589528@qq.com>
2021-06-11 12:15:59 +0800

    观察者模式猫叫案例
    使用观察者模式实现猫叫唤醒主人，惊走老鼠案例！

---
+++ 观察者模式/ObserverDemo/ObserverDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //实例化目标
+            Subject subject = new Cat();
+            //实例化观察者
+            IObserver mouse = new Mouse();
+            IObserver master = new Master();
+            //将观察者放到集合中
+            subject.AddObserver(obs: mouse);
+            subject.AddObserver(obs: master);
+            subject.Call();
+            Console.Read();
+        }
+    }
+}
